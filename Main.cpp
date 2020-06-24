3a2894d4a1a31959a84593b5e4788cc9f61768e4
Isherf <isherftheg@gmail.com>
2020-06-24 21:35:57 +0630

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the" << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n ";
+
+	return 0;
+}
